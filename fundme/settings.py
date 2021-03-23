3e1ecdd27ce1bd1d8b553ee8b91eb38400ba4552
Jelmah04 <Jelmah04@gmail.com>
2021-03-23 16:24:10 +0100

    third commit

--- fundme/settings.py
+++ fundme/settings.py
@@ -127,7 +127,10 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 AUTH_USER_MODEL = 'web.User'
 
